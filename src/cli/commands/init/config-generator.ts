18e87ca0ef6f16d3a0f87d42de01b132ea258541
Koji Wakayama <koji@codersociety.com>
2025-12-06 12:17:22 +0100

    chore: wip

--- src/cli/commands/init/config-generator.ts
+++ src/cli/commands/init/config-generator.ts
@@ -120,7 +120,7 @@ export async function createPackageJson(
     dependencies: {
       react: "^19.0.0",
       "react-dom": "^19.0.0",
-      veryfront: "^0.0.31",
+      veryfront: "^0.0.32",
       zod: "^3.24.0",
     },
   };
