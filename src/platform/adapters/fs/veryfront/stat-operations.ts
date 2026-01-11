83c2db20eae8692fc8b667325d8dc4e1edfb4720
Koji Wakayama <koji@codersociety.com>
2026-01-11 23:53:12 +0100

    Revert "chore: loose schema strictness"
    This reverts commit 68cd851763ac2adc0614806e564ebeb0a503a150.

--- src/platform/adapters/fs/veryfront/stat-operations.ts
+++ src/platform/adapters/fs/veryfront/stat-operations.ts
@@ -54,7 +54,7 @@ export class StatOperations {
       logger.debug("[StatOperations] stat found file", { normalizedPath });
       const info: FileInfo = {
         size: file.size,
-        mtime: file.updated_at ? new Date(file.updated_at) : new Date(),
+        mtime: new Date(file.updated_at),
         isDirectory: false,
         isFile: true,
         isSymlink: false,
