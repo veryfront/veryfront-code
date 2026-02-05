2c7ce615f7d3fdadfd70026dd93e56b8d8e13b54
ariskemper <aris.github@gmail.com>
2026-02-05 17:09:07 +0100

    fix: api.schema.test.ts

--- src/platform/adapters/veryfront-api-client/schemas/api.schema.test.ts
+++ src/platform/adapters/veryfront-api-client/schemas/api.schema.test.ts
@@ -12,7 +12,7 @@ import {
   ProjectFileSchema,
   ProjectSchema,
   ReleaseFileListItemSchema,
-} from "./schemas/index.ts";
+} from "./index.ts";
 
 describe("schemas", () => {
   describe("ProjectSchema", () => {
