b87d661f89635dcb74971610b0798e37b7452d3e
ariskemper <aris.github@gmail.com>
2026-02-10 16:57:59 +0100

    fix: re-export __resetRSCHandlerForTests dropped during barrel cleanup

--- src/server/services/rsc/endpoints/index.ts
+++ src/server/services/rsc/endpoints/index.ts
@@ -7,6 +7,6 @@
 export { parseActionBody } from "./action-parser.ts";
 export { handleActionRequest } from "./action-handler.ts";
 export { handleRSCEndpoint } from "./endpoint-router.ts";
-export { getRSCHandler } from "./handler-registry.ts";
+export { __resetRSCHandlerForTests, getRSCHandler } from "./handler-registry.ts";
 export { handleClientScript, handleDomScript } from "./script-handlers.ts";
 export type { ActionBody, ActionRequestParams, RSCEndpointParams } from "./types.ts";
